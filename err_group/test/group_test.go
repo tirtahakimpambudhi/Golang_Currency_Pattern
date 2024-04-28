ca2ac1a8f83b3d14a56d0b6306e0c27aaafdb094
tirtahakimpambudhi <awebthp@gmail.com>
2024-04-28 16:03:17 +0700

    test(errgroup) : add unit test TestErrGroup function

--- err_group/test/group_test.go
+++ err_group/test/group_test.go
@@ -1,13 +1,20 @@
 package test
 
 import (
+	"context"
 	errgroup "go_errgroup"
 	"testing"
-
 )
 
 func TestWaitGroup(t *testing.T) {
 	wait := errgroup.WaitGroup()
+	<- wait
+	t.Log("finish")
+}
+
+func TestErrGroup(t *testing.T) {
+	ctx := context.Background()
+	wait := errgroup.ErrGroup(ctx)
 	<- wait
 	t.Log("finish")
 }
